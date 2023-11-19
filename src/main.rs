110dc414e2e5d010a03d279004b10cc990ab4fdc
0xRuFFy <konstantinopora@gmail.com>
2023-11-19 15:04:42 +0100

    added  smaller and  easier test file

--- src/main.rs
+++ src/main.rs
@@ -14,46 +14,59 @@ fn main() {
     );
     println!("{}", sudoku);
 
-    // let solver = backtracking_bf::BBFS::new();
+    // let mut solver = backtracking_bf::BBFS::new();
     let mut solver = backtracking_ds::BDSS::new();
     let start = Instant::now();
     sudoku.solve(&mut solver);
     println!("Time elapsed: {:?}", start.elapsed());
     println!("{}", sudoku);
 
-    // let mut sudoku = Sudoku::new();
-    // let solver = backtracking_ds::BDSS::new();
-    // let file = fs::read_to_string("data/bench.txt").expect("Unable to read file");
-    // let mut lines = file.lines();
-    // let count = lines.next().map(|l| l.parse::<usize>().unwrap()).unwrap();
+    // let mut sudoku = board::Sudoku::new();
+    // // let mut solver = backtracking_bf::BBFS::new();
+    // let mut solver = backtracking_ds::BDSS::new();
+    // let file = std::fs::read_to_string("data/easy50.txt").expect("Unable to read file");
+    // let lines = file.lines();
+    // let count = lines.clone().count();
     // println!("Running {} benchmarks.", count);
     // let mut total = 0;
+    // use std::io::{self, Write};
+    // let start = Instant::now();
     // for line in lines {
     //     sudoku.load_board_from_str(line);
-    //     sudoku.solve(&solver);
+    //     println!("{}", sudoku);
+    //     sudoku.solve(&mut solver);
+    //     println!("{}", sudoku);
     //     total += 1;
     //     print!("{:<6} / {:<6}\r", total, count);
     //     io::stdout().flush().unwrap();
     // }
+    // println!("Time elapsed: {:?}", start.elapsed());
+
+
+    // let file = std::fs::read_to_string("data/bench.txt").expect("Unable to read file");
+    // let mut lines = file.lines();
+    // let count = lines.next().map(|l| l.parse::<usize>().unwrap()).unwrap();
 
     // let mut i = 0;
-    // let (tx, rx) = mpsc::channel();
-    // // let count = 100;
+    // let (tx, rx) = std::sync::mpsc::channel();
+    // // let count = 10;
     // let threads: Vec<_> = (0..count)
     //     .map(|_| tx.clone())
     //     .map(|txc| {
-    //         let mut sudoku = Sudoku::new();
+    //         let mut sudoku = board::Sudoku::new();
     //         sudoku.load_board_from_str(lines.next().unwrap());
-    //         let solver = backtracking_ds::BDSS::new();
+    //         let mut solver = backtracking_ds::BDSS::new();
     //         i += 1;
-    //         thread::spawn(move || {
-    //             sudoku.solve(&solver);
+    //         std::thread::spawn(move || {
+    //             sudoku.solve(&mut solver);
     //             txc.send(i).unwrap();
     //         })
     //     })
     //     .collect();
 
     // let mut total = 0;
+
+    // use std::io::{self, Write};
     // for _ in rx {
     //     total += 1;
     //     print!("{:<6} / {:<6}\r", total, count);
