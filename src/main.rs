fb544869aaa5bf70067a454d3880a7d1a534a34e
0xRuFFy <konstantinopora@gmail.com>
2023-11-21 13:40:04 +0100

    testing around with smart_solver

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,13 @@ mod solver;
 
 fn main() {
     let mut sudoku = board::Sudoku::new();
-    // sudoku.load_board_from_str("000000010400000000020000000000050407008000300001090000300400200050100000000806000");
-    sudoku.load_board_from_str("003020600900305001001806400008102900700000008006708200002609500800203009005010300");
+    // sudoku.load_board_from_str("000000010400000000020000000000050407008000300001090000300400200050100000000806000", '0');
+    // sudoku.load_board_from_str("003020600900305001001806400008102900700000008006708200002609500800203009005010300", '0');
+    // sudoku.load_board_from_str("200080300060070084030500209000105408000000000402706000301007040720040060004010003", '0');
+    sudoku.load_board_from_str(
+        "043080250600000000000001094900004070000608000010200003820500000000000005034090710",
+        '0',
+    );
     println!("{}", sudoku);
 
     // let mut solver = backtracking_bf::BBFS::new();
@@ -34,15 +39,16 @@ fn main() {
     // use std::io::{self, Write};
     // let start = Instant::now();
     // for line in lines {
-    //     println!("{}", line);
-    //     sudoku.load_board_from_str(line);
+    //     // println!("{}", line);
+    //     sudoku.load_board_from_str(line, '0');
     //     sudoku.solve(&mut solver);
     //     total += 1;
-    //     print!("{:<6} / {:<6}\r", total, count);
-    //     io::stdout().flush().unwrap();
+    //     // print!("{:<6} / {:<6}\r", total, count);
+    //     // io::stdout().flush().unwrap();
     // }
     // println!("Time elapsed: {:?}", start.elapsed());
 
+    // let start = Instant::now();
     // let file = std::fs::read_to_string("data/bench.txt").expect("Unable to read file");
     // let mut lines = file.lines();
     // let count = lines.next().map(|l| l.parse::<usize>().unwrap()).unwrap();
@@ -54,8 +60,9 @@ fn main() {
     //     .map(|_| tx.clone())
     //     .map(|txc| {
     //         let mut sudoku = board::Sudoku::new();
-    //         sudoku.load_board_from_str(lines.next().unwrap());
-    //         let mut solver = backtracking_ds::BDSS::new();
+    //         sudoku.load_board_from_str(lines.next().unwrap(), '0');
+    //         // let mut solver = backtracking_ds::BDSS::new();
+    //         let mut solver = smart_solver::SmartSolver::new();
     //         i += 1;
     //         std::thread::spawn(move || {
     //             sudoku.solve(&mut solver);
@@ -72,6 +79,7 @@ fn main() {
     //     print!("{:<6} / {:<6}\r", total, count);
     //     io::stdout().flush().unwrap();
     //     if total == count {
+    //         println!("Time elapsed: {:?}", start.elapsed());
     //         break;
     //     }
     // }
