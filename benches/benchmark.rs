d5d208f26588daa24be9a4fd9852dc785243c3fb
0xRuFFy <konstantinopora@gmail.com>
2023-11-21 13:39:44 +0100

    added smartsolver to benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -12,12 +12,30 @@ fn backtracking_ds_benchmark(c: &mut Criterion) {
     group.measurement_time(std::time::Duration::from_secs(24));
     group.bench_function("backtracking_ds", |b| {
         b.iter(|| {
-            sudoku.load_board_from_str(black_box(BOARD));
+            sudoku.load_board_from_str(black_box(BOARD), '0');
             sudoku.solve(&mut solver);
         })
     });
     group.finish();
 }
 
-criterion_group!(benches, backtracking_ds_benchmark);
+fn smart_solver_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("smart_solver");
+
+    let mut sudoku = Sudoku::new();
+    let mut solver = sudoku::SmartSolver::new();
+    const BOARD: &str = "000000010400000000020000000000050407008000300001090000300400200050100000000806000";
+
+    group.significance_level(0.1).sample_size(500);
+    group.measurement_time(std::time::Duration::from_secs(24));
+    group.bench_function("smart_solver", |b| {
+        b.iter(|| {
+            sudoku.load_board_from_str(black_box(BOARD), '0');
+            sudoku.solve(&mut solver);
+        })
+    });
+    group.finish();
+}
+
+criterion_group!(benches, backtracking_ds_benchmark, smart_solver_benchmark);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod backtracking_bf; // backtracking brute force
 mod backtracking_ds; // backtracking dynamic selection
 mod board;
+mod smart_solver; // smart solver
 mod solver;
 
 pub use backtracking_bf::BBFS;
 pub use backtracking_ds::BDSS;
 pub use board::Sudoku;
+pub use smart_solver::SmartSolver;
 pub use solver::Solver;
